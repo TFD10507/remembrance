2f90545821d39f96332dd084ddf38640b3afa59e
TFD10507 <ydolemlinn@gmail.com>
2022-02-06 14:55:12 +0800

    購物車完成

--- js/course_js.js
+++ js/course_js.js
@@ -1,37 +1,37 @@
 "use strict";
+// =============== 課程 ======================
+let link_bt = document.getElementsByClassName("linkbt")[0];
+let fade_in = document.getElementsByClassName('hand-merit-fade')[0];
+let close_bt = document.getElementsByClassName("closebt")[0];
 
-        let link_bt = document.getElementsByClassName("linkbt")[0];
-        let fade_in = document.getElementsByClassName('hand-merit-fade')[0];
-        let close_bt = document.getElementsByClassName("closebt")[0];
-
-        link_bt.onclick = function () {
-            fade_in.style.display = 'block';
-        }
-        close_bt.onclick = function () {
-            fade_in.style.display = 'none';
-        }
-
-
-        let link_bt_1 = document.getElementsByClassName("linkbt")[1];
-        let fade_in_1 = document.getElementsByClassName('hand-merit-fade')[1];
-        let close_bt_1 = document.getElementsByClassName("closebt")[1];
-
-        link_bt_1.onclick = function () {
-            fade_in_1.style.display = 'block';
-        }
-        close_bt_1.onclick = function () {
-            fade_in_1.style.display = 'none';
-        }
-        
+link_bt.onclick = function() {
+    fade_in.style.display = 'block';
+}
+close_bt.onclick = function() {
+    fade_in.style.display = 'none';
+}
 
 
-        let link_bt_2 = document.getElementsByClassName("linkbt")[2];
-        let fade_in_2 = document.getElementsByClassName('hand-merit-fade')[2];
-        let close_bt_2 = document.getElementsByClassName("closebt")[2];
+let link_bt_1 = document.getElementsByClassName("linkbt")[1];
+let fade_in_1 = document.getElementsByClassName('hand-merit-fade')[1];
+let close_bt_1 = document.getElementsByClassName("closebt")[1];
 
-        link_bt_2.onclick = function () {
-            fade_in_2.style.display = 'block';
-        }
-        close_bt_2.onclick = function () {
-            fade_in_2.style.display = 'none';
-        }
+link_bt_1.onclick = function() {
+    fade_in_1.style.display = 'block';
+}
+close_bt_1.onclick = function() {
+    fade_in_1.style.display = 'none';
+}
+
+
+
+let link_bt_2 = document.getElementsByClassName("linkbt")[2];
+let fade_in_2 = document.getElementsByClassName('hand-merit-fade')[2];
+let close_bt_2 = document.getElementsByClassName("closebt")[2];
+
+link_bt_2.onclick = function() {
+    fade_in_2.style.display = 'block';
+}
+close_bt_2.onclick = function() {
+    fade_in_2.style.display = 'none';
+}
