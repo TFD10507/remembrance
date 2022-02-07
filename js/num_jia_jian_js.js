1391de28ecc763771add5daee64cf320e36c7e31
TFD10507 <ydolemlinn@gmail.com>
2022-02-07 13:12:23 +0800

    刪除js

--- js/num_jia_jian_js.js
+++ js/num_jia_jian_js.js
@@ -1,36 +1,47 @@
 "use strict";
-// ===============================================
+// =================== 數量加減 ======================
 var num_jia = document.getElementsByClassName("num-jia")[0];
-        var num_jian = document.getElementsByClassName("num-jian")[0];
-        var input_num = document.getElementsByClassName("input-num")[0];
+var num_jian = document.getElementsByClassName("num-jian")[0];
+var input_num = document.getElementsByClassName("input-num")[0];
 
-        num_jia.addEventListener("click", function () {
-            input_num.value = parseInt(input_num.value) + 1;
-        });
+num_jia.addEventListener("click", function () {
+    input_num.value = parseInt(input_num.value) + 1;
+});
 
-        num_jian.addEventListener("click", function () {
-            if (input_num.value <= 1) {
-                input_num.value = 1;
-            } else {
+num_jian.addEventListener("click", function () {
+    if (input_num.value <= 1) {
+        input_num.value = 1;
+    } else {
 
-                input_num.value = parseInt(input_num.value) - 1;
-            }
-        });
+        input_num.value = parseInt(input_num.value) - 1;
+    }
+});
 
-        var num_jia_1 = document.getElementsByClassName("num-jia")[1];
-        var num_jian_1 = document.getElementsByClassName("num-jian")[1];
-        var input_num_1 = document.getElementsByClassName("input-num")[1];
+var num_jia_1 = document.getElementsByClassName("num-jia")[1];
+var num_jian_1 = document.getElementsByClassName("num-jian")[1];
+var input_num_1 = document.getElementsByClassName("input-num")[1];
 
-        num_jia_1.addEventListener("click", function () {
-            input_num_1.value = parseInt(input_num_1.value) + 1;
-        });
+num_jia_1.addEventListener("click", function () {
+    input_num_1.value = parseInt(input_num_1.value) + 1;
+});
 
-        num_jian_1.addEventListener("click", function () {
-            if (input_num_1.value <= 1) {
-                input_num_1.value = 1;
-            } else {
+num_jian_1.addEventListener("click", function () {
+    if (input_num_1.value <= 1) {
+        input_num_1.value = 1;
+    } else {
+        input_num_1.value = parseInt(input_num_1.value) - 1;
+    }
+});
 
-                input_num_1.value = parseInt(input_num_1.value) - 1;
-            }
-        });
 
+
+// ==================== 刪除 =========================
+
+var del = document.getElementsByClassName("del");
+
+for (let i = 0; i < del.length; i++) {
+    del[i].addEventListener("click", function(){
+        let closest_ul = this.closest("ul");
+        closest_ul.remove();
+    });
+}
