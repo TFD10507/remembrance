5262d632ad392b203806eb732302ae48d34ada31
TFD10507 <ydolemlinn@gmail.com>
2022-02-09 20:39:02 +0800

    導覽列提醒

--- js/course_js.js
+++ js/course_js.js
@@ -1,19 +1,27 @@
 "use strict";
 // =============== 課程 ======================
 
+// 綁定圖片連結
 let link_bt = document.getElementsByClassName("linkbt");
+// 綁定燈箱表單
 let fade_in = document.getElementsByClassName('black');
+// 綁定關閉按鈕
 let close_bt = document.getElementsByClassName("closebt");
-// let content = document.getElementsByClassName("hand-merit-fade")[0];
+
 
 for (let i = 0; i < 3; i++) {
-
+    // 綁定事件：點擊圖片連結時，燈箱出現
     link_bt[i].addEventListener("click", function () {
         fade_in[i].style.display = 'block';
     });
+    
+    // 綁定事件：點擊關閉按鈕時，燈箱消失
     close_bt[i].addEventListener("click", function () {
         fade_in[i].style.display = 'none';
     });
+
+    // 綁定事件：如果點擊目標事件 = fade_in時，燈箱消失
+    // 點擊燈箱之外的區域，燈箱消失
     fade_in[i].addEventListener("click", function (event) {
         if(event.target == fade_in[i]){
             fade_in[i].style.display = 'none';
