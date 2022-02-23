c1e77c9f71b15c64d515a8731ca758a8c9376954
TFD10507 <ydolemlinn@gmail.com>
2022-02-23 13:43:36 +0800

    購物車

--- js/num_jia_jian_js.js
+++ js/num_jia_jian_js.js
@@ -1,9 +1,9 @@
 "use strict";
 // =================== 數量加減 ======================
-var num_jia = document.getElementsByClassName("num-jia")[0];
-var num_jian = document.getElementsByClassName("num-jian")[0];
-var input_num = document.getElementsByClassName("input-num")[0];
 
+let num_jia = document.getElementsByClassName("num-jia")[0];
+let num_jian = document.getElementsByClassName("num-jian")[0];
+let input_num = document.getElementsByClassName("input-num")[0];
 num_jia.addEventListener("click", function () {
     input_num.value = parseInt(input_num.value) + 1;
 });
@@ -17,9 +17,9 @@ num_jian.addEventListener("click", function () {
     }
 });
 
-var num_jia_1 = document.getElementsByClassName("num-jia")[1];
-var num_jian_1 = document.getElementsByClassName("num-jian")[1];
-var input_num_1 = document.getElementsByClassName("input-num")[1];
+let num_jia_1 = document.getElementsByClassName("num-jia")[1];
+let num_jian_1 = document.getElementsByClassName("num-jian")[1];
+let input_num_1 = document.getElementsByClassName("input-num")[1];
 
 num_jia_1.addEventListener("click", function () {
     input_num_1.value = parseInt(input_num_1.value) + 1;
@@ -37,11 +37,12 @@ num_jian_1.addEventListener("click", function () {
 
 // ==================== 刪除 =========================
 
-var del = document.getElementsByClassName("del");
+let del = document.getElementsByClassName("del");
 
 for (let i = 0; i < del.length; i++) {
-    del[i].addEventListener("click", function(){
+    del[i].addEventListener("click", function () {
         let closest_ul = this.closest("ul");
+        console.log(closest_ul);
         closest_ul.remove();
     });
 }
