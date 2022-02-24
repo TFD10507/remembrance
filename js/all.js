8f3e54137deb8a83ff708b361b4167e901297b72
TFD10507 <ydolemlinn@gmail.com>
2022-02-24 22:45:56 +0800

    loading畫面完成

---
+++ js/all.js
@@ -0,0 +1,39 @@
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+const sliderItems = document.querySelectorAll('.slide-in');
+// console.log(sliderImages);
+
+function checkSlide() {
+    sliderItems.forEach(sliderItem => {
+        // 取得圖片1/2高度的定位點（卷軸垂直位移量＋視窗高度）- 1/2圖片高度
+        const slideInAt = (window.scrollY + window.innerHeight) - (sliderItem.offsetHeight / 4);
+        // 取得圖片底部定位點（利用圖片頂部定位點+圖片高度取得）
+        const imageBottom = sliderItem.offsetTop + sliderItem.offsetHeight;
+        // 判斷視窗是否已經超過圖片高度一半
+        const isHalfShown = slideInAt > sliderItem.offsetTop;
+        // 判斷滾動範圍是否已經超過圖片底部（卷軸垂直位移量）
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        // 判斷是否超過圖片一半高，且視窗尚未超過圖片底部來增加或移除css效果
+        if (isHalfShown && isNotScrolledPast) {
+            sliderItem.classList.add('active');
+        } else {
+            sliderItem.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
--- js/course_js.js
+++ js/course_js.js
@@ -24,22 +24,22 @@ for (let i = 0; i < 3; i++) {
 
     // 綁定事件：如果點擊目標事件 = fade_in時，燈箱消失
     // 點擊燈箱之外的區域，燈箱消失
-
     //寫法1
-    // fade_in[i].addEventListener("click", function (event) {
-    //     if (event.target == fade_in[i]) {
-    //         fade_in[i].style.display = 'none';
-    //     }
-    // });
+    fade_in[i].addEventListener("click", function (event) {
+        if (event.target == fade_in[i]) {
+            fade_in[i].style.display = 'none';
+        }
+    });
+
     //寫法2
-    fade_in[i].addEventListener("click",function(e){
-        e.stopPropagation();
-        fade_in[i].style.display = 'none';
-    });
-    hand[i].addEventListener("click",function(e){
-        e.stopPropagation();
-        fade_in[i].style.display = 'block';
-    });
+    // fade_in[i].addEventListener("click",function(e){
+    //     e.stopPropagation();
+    //     fade_in[i].style.display = 'none';
+    // });
+    // hand[i].addEventListener("click",function(e){
+    //     e.stopPropagation();
+    //     fade_in[i].style.display = 'block';
+    // });
 }
 
 
@@ -50,9 +50,9 @@ $(document).ready(function () {
     $(window).scroll(function () {
         b = $(this).scrollTop();   //頁面滾動的高度  
         c = $(".course-container").offset().top;    //元素距離文件(document)頂部的高度  
-        console.log(a);  
-        console.log(b);  
-        console.log(c);  
+        // console.log(a);  
+        // console.log(b);  
+        // console.log(c);  
         if ( b > 350) {
             $(".nav-reserve"). addClass("-on")
             $(".nav-course"). removeClass("-on")
@@ -65,62 +65,7 @@ $(document).ready(function () {
 
 
 
-function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  };
-  
-  const sliderImages = document.querySelectorAll('.slide-in');
-  
-  function checkSlide(e) {
-    sliderImages.forEach(sliderImage => {
-      const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
-      const imageBottom = sliderImage.offsetTop + sliderImage.height;
-      const isHalfShown = slideInAt > sliderImage.offsetTop;
-      const isNotScrolledPast = window.scrollY < imageBottom;
-  
-      if(isHalfShown && isNotScrolledPast) {
-        sliderImage.classList.add('active');
-      } else {
-        sliderImage.classList.remove('active');
-      }
-    });
-  }
-  
-  window.addEventListener('scroll', debounce(checkSlide));
 
-/*
- // append calendar with #schedule -- schedule.js
- if (document.getElementById('schedule') !== null) createCalendar();
-
- let footerAbout = document.querySelectorAll('footer .col.about')[0];
- footerAbout.addEventListener('click', function(){
-     footerAbout.style.animation = 'rotate 1s';
- })
- footerAbout.addEventListener('animationend', function(e){
-     // console.log(document.cookie);
-     let cookie = document.cookie.split(';');
-     if (cookie.indexOf(' footer=check') === -1) {
-         alert('找到一張9折優惠券!');
-         document.cookie = 'footer=check';
-     }
-     else {
-         alert('這裡沒有東西了!');
-     }
-     e.target.removeAttribute('style');
- })
-
- */
 
 
 
