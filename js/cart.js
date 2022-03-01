5a9bb14275639e71cd776fd47e0c1d6001734139
TFD10507 <ydolemlinn@gmail.com>
2022-03-02 00:59:10 +0800

    購物車啞光處理字體色彩大小調整

--- js/cart.js
+++ js/cart.js
@@ -31,6 +31,7 @@ function get_takes() {
     if (carts != null) {
         for (let i = 0; i < carts.length; i++) {
             let list_html = `
+            <ul class="cart-content">
             <li class="cart-content-flex" prd_id="${carts[i].prd_id}" prd_index="${i}">
             <div class="img">
                 <img src="./source/product/ring/s.jpg" alt="">
@@ -57,15 +58,15 @@ function get_takes() {
                 <i class="far fa-heart"></i>
             </a>
         </li>
+        </ul>
         `
             //定義 ul_task_list 
-            let ul_task_list = document.getElementsByClassName("cart-content")[0];
-            ul_task_list.insertAdjacentHTML("afterbegin", list_html);
+            let ul_task_list = document.getElementsByClassName("sp-cart-header")[0];
+            ul_task_list.insertAdjacentHTML("afterend", list_html);
         }
 
     }
 }
-
 
 //   商品頁的加入購物車
 let add_cart_btn = document.getElementsByClassName("prd-btn")[0];
@@ -112,7 +113,6 @@ if (add_cart_btn != null && prd_wrp != null) {
 
 };
 
-
 //切到購物車頁面
 document.addEventListener("DOMContentLoaded", function (e) {
     // console.log(document.title);
@@ -136,99 +136,52 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 let updata = carts;
                 updata.splice(ul_task_list_index, 1);
                 localStorage.setItem("carts", JSON.stringify(updata));
-                // cart_count_renew();
                 cart_total_renew();
                 ul_task_list.classList.add("fade_out");
                 setTimeout(function () {
                     ul_task_list.remove();
-                }, 800);
+                }, 500);
                 if (localStorage.carts == "[]") {
                     time_to_shop();
                 }
             }
 
-            //修改數量
-            // let cart_prd_count = document.getElementsByClassName("cart_prd_count");
-            // console.log( cart_prd_count);
-
             //按下+號
-            if ((e.target.querySelector(".fa-plus") && e.target.closest(".cart_prd_count")) || e.target.classList.contains("fa-plus")) {
-                // console.log("+")
-                if (e.target.parentElement.classList == "prd_count") {
-                    let prd_countel = e.target.closest(".cart_prd_count");
-                    prd_countel = prd_countel.querySelector("input");
-                    // console.log(prd_count)
-                    prd_countel.value++;
+            if ((e.target.querySelector(".num-jia") && e.target.closest(".count")) || e.target.classList.contains("num-jia")) {
+                let prd_countel = e.target.parentElement.closest(".count");
+                let cart_content = e.target.parentElement.closest(".cart-content");
+                prd_countel = prd_countel.querySelector("input");
+                prd_countel.value++;
 
-                    let prd_index = e.target.closest(".cart_prd").getAttribute("prd_index");
-                    // console.log(prd_index);
-                    //更新購物車
-                    let carts = JSON.parse(localStorage.getItem("carts"));
-                    // console.log(carts);
-                    carts[prd_index].prd_count = prd_countel.value;
-                    // console.log(carts[prd_index]);
-                    localStorage.setItem("carts", JSON.stringify(carts));
+                //更新購物車
+                let prd_index = cart_content.querySelector(".cart-content-flex").getAttribute("prd_index");
+                let carts = JSON.parse(localStorage.getItem("carts"));
+                carts[prd_index].prd_count = prd_countel.value;
+                localStorage.setItem("carts", JSON.stringify(carts));
 
-                } else {
-                    let prd_countel = e.target.parentElement.closest(".cart_prd_count");
-                    console.log(prd_countel);
-                    console.log(e.target);
-                    prd_countel = prd_countel.querySelector("input");
-                    prd_countel.value++;
-                    //更新購物車
-                    let prd_index = e.target.closest(".cart_prd").getAttribute("prd_index");
-                    let carts = JSON.parse(localStorage.getItem("carts"));
-                    carts[prd_index].prd_count = prd_countel.value;
-                    localStorage.setItem("carts", JSON.stringify(carts));
-                }
                 cart_total_renew();
             }
 
             // 按下-號
-            if ((e.target.querySelector(".fa-minus") && e.target.closest(".cart_prd_count")) || e.target.classList.contains("fa-minus")) {
-                // console.log("-");
-                if (e.target.parentElement.classList == "prd_count") {
-                    let prd_countel = e.target.closest(".cart_prd_count");
-                    prd_countel = prd_countel.querySelector("input");
-                    if (prd_countel.value > 1) {
-                        prd_countel.value--;
-                    }
-                    let prd_index = e.target.closest(".cart_prd").getAttribute("prd_index");
-                    let carts = JSON.parse(localStorage.getItem("carts"));
-                    carts[prd_index].prd_count = prd_countel.value;
-                    localStorage.setItem("carts", JSON.stringify(carts));
-                } else {
-                    let prd_countel = e.target.parentElement.closest(".cart_prd_count");
-                    prd_countel = prd_countel.querySelector("input");
-                    if (prd_countel.value > 1) {
-                        prd_countel.value--;
-                    }
-                    let prd_index = e.target.closest(".cart_prd").getAttribute("prd_index");
-                    let carts = JSON.parse(localStorage.getItem("carts"));
-                    carts[prd_index].prd_count = prd_countel.value;
-                    localStorage.setItem("carts", JSON.stringify(carts));
+            if ((e.target.querySelector(".num-jian") && e.target.closest(".count")) || e.target.classList.contains("num-jian")) {
+                let prd_countel = e.target.parentElement.closest(".count");
+                let cart_content = e.target.parentElement.closest(".cart-content");
+                prd_countel = prd_countel.querySelector("input");
+                if (prd_countel.value > 1) {
+                    prd_countel.value--;
                 }
+
+                //更新購物車
+                let prd_index = cart_content.querySelector(".cart-content-flex").getAttribute("prd_index");
+                let carts = JSON.parse(localStorage.getItem("carts"));
+                carts[prd_index].prd_count = prd_countel.value;
+                localStorage.setItem("carts", JSON.stringify(carts));
+
                 cart_total_renew();
             }
-
-            // var count_nub = document.getElementsByClassName("prd_nub")[0];
-            // console.log(count_nub.value);
-            // count_nub.addEventListener("keyup", function (k) {
-            //     var str = (k.target.value).replace(/\D/g, "");
-            //     k.target.value = str;
-            // })
-            // count_nub.addEventListener("change", function () {
-            //     let prd_index = e.target.closest(".cart_prd").getAttribute("prd_index");
-            //     let carts = JSON.parse(localStorage.getItem("carts"));
-            //     carts[prd_index].prd_count = count_nub.value;
-            //     localStorage.setItem("carts", JSON.stringify(carts));
-            //     cart_total_renew();
-            // });
-
-
         });
 
-        // 購物車總結品項、金額
+        // 購物車內的總結金額
         if (cart_count_el.length != 0) {
             cart_total_renew();
         }
@@ -238,21 +191,21 @@ document.addEventListener("DOMContentLoaded", function (e) {
 });
 
 
-// 商品總價
+// 計算商品總價
 function cart_total_renew() {
     // console.log("a")
     let cart_total_el = document.getElementsByClassName("cart-total-price")[0];
     let cart_total_price = cart_total_el.querySelector(" li:first-child > span:nth-child(2)");
     let cart_total_price2 = cart_total_el.querySelector(" li:nth-child(4) > span:nth-child(2)");
-    console.log(cart_total_price);
+    // console.log(cart_total_price);
     let task = JSON.parse(localStorage.getItem("carts"));
     let local_total_price = 0;
     for (let i = 0; i < task.length; i++) {
         // console.log("商品數量" + task[i].prd_count + ";商品價錢" + task[i].prd_price + ";商品金額" + task[i].prd_count *task[i].prd_price);
         local_total_price += (parseInt(task[i].prd_price) * parseInt(task[i].prd_count));
     }
-    console.log(local_total_price);
+    // console.log(local_total_price);
     cart_total_price.innerText = local_total_price;
     cart_total_price2.innerText = local_total_price + 80;
-    
+
 }
